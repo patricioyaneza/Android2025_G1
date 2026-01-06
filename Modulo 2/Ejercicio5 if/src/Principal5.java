3d11911cbe72c64001ed692ff61155d0eb42611f
patricioyaneza <pa.yaneza@outlook.cl>
2026-01-05 21:58:04 -0300

    update

---
+++ Modulo 2/Ejercicio5 if/src/Principal5.java
@@ -0,0 +1,36 @@
+
+public class Principal5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int dia = 3;  // lunes 1 ... dom 7
+		
+		switch(dia) {
+		case 1:
+			System.out.println("Lunes");
+			break;
+		case 2:
+			System.out.println("Martes");
+			break;
+		case 3:
+			System.out.println("Miércoles");
+			break;
+		case 4:
+			System.out.println("Jueves");
+			break;
+		case 5:
+			System.out.println("Viernes");
+			break;
+		case 6:
+			System.out.println("Sábado");
+			break;
+		case 7:
+			System.out.println("Domingo");
+			break;
+			
+		}
+		System.out.println("fin");
+		
+	}
+
+}
