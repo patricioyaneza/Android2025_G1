309b8761181da83184445bff80797c5210df7f80
patricioyaneza <pa.yaneza@outlook.cl>
2026-01-07 21:23:41 -0300

    update

---
+++ Modulo 2/Ejercicio6 String/src/Principal.java
@@ -0,0 +1,36 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombre = " Juan ";
+		
+		System.out.println("Contar caracteres: " + nombre.length()); // len
+		
+		System.out.println("Mayúscula: " + nombre.toUpperCase()); // upper toUpper
+		System.out.println("minúscula: " + nombre.toLowerCase());
+		
+		System.out.println("valor :" + nombre + ".");
+		System.out.println("valor :" + nombre.trim() + ".");
+    	
+		System.out.println("1er parrafo \n2do parrafo \n3er parrafo");
+		
+		String frase = "Estoy cansado jefe.";
+		System.out.println("indexOf		: " + frase.indexOf("jefe"));
+		System.out.println("indexOf		: " + frase.indexOf("a"));
+		System.out.println("lastIndexOf	: " + frase.lastIndexOf("a"));
+		System.out.println("charAt		: " + frase.charAt(4));
+		
+		System.out.println("SubString	: " + frase.substring(6));
+		System.out.println("SubString	: " + frase.substring(6, 13) + ".");
+		
+		/* Ejercicio 9:
+		 * 
+		 * 
+		 * 
+		 */
+		
+		
+	}
+
+}
