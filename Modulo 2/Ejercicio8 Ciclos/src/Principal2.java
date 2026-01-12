5659aa31879ffbf8f481b1b1ddb18ad4168dd7b2
patricioyaneza <pa.yaneza@outlook.cl>
2026-01-12 20:42:27 -0300

    update

---
+++ Modulo 2/Ejercicio8 Ciclos/src/Principal2.java
@@ -0,0 +1,14 @@
+
+public class Principal2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		Ejercicio 16:
+			Solicitar al usuario la cantidad de notas a ingresarlas. 
+			Obtener promedio y mostrar si aprobo o no.
+			Usar arreglos para guardar las notas.
+		*/
+	}
+
+}
