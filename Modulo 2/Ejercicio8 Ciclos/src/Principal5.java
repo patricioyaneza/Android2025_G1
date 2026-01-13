e9589e74780032d581483595fe59a8faa6b2a848
patricioyaneza <pa.yaneza@outlook.cl>
2026-01-12 22:23:44 -0300

    update

--- Modulo 2/Ejercicio8 Ciclos/src/Principal5.java
+++ Modulo 2/Ejercicio8 Ciclos/src/Principal5.java
@@ -5,6 +5,10 @@ public class Principal5 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner leer = new Scanner(System.in);
+		
+		int num1 = 0;
+		int num2 = 0;
+		int resultado = 0;
 		int opcion = -1;
 		while(opcion != 0)
 		{
@@ -15,25 +19,46 @@ public class Principal5 {
 			System.out.print("Seleccione la opción: ");
 			opcion = leer.nextInt();
 			
+			// validar que este dentro del rango del menu
+			if(opcion < 0 || opcion > 2)
+			{
+				System.out.println("**********************************");
+				System.out.println("La opción no es válida");
+				System.out.println("Presione enter para continuar");
+				System.out.println("**********************************");
+				leer.nextLine();
+				leer.nextLine();
+				continue;
+			}
+			
 			if(opcion == 0)
-				break;
+				break;		
 /*
  *  Solicitar 2 numeros y sumar o restar de acuerdo a lo que el 
  *  usuario seleccionó
  * 			
  */
+			System.out.println("Ingrese primer número");
+			num1 = leer.nextInt();
+
+			System.out.println("Ingrese segundo número");
+			num2 = leer.nextInt();
+			
 			if(opcion == 1)
 			{
 				System.out.println("===== Sumar ======");
-				
-				
+				resultado = num1 + num2;				
 			}
 			else if(opcion == 2)
 			{
 				System.out.println("===== Restar ======");
+				resultado = num1 - num2;				
 			}
-			System.out.println("Presione C y enter para continuar...");
-			leer.next();
+			
+			System.out.println("El resultado es: " + resultado);	
+			System.out.println("Presione enter para continuar...");
+			leer.nextLine();
+			leer.nextLine();
 		}
 		
 		System.out.println("________ Fin de la aplicación_________");
