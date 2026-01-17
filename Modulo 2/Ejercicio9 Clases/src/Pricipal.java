3ccb5eb5a9a4f16af80dfeb53914be99fbe918d9
patricioyaneza <pa.yaneza@outlook.cl>
2026-01-16 22:15:44 -0300

    update

--- Modulo 2/Ejercicio9 Clases/src/Pricipal.java
+++ Modulo 2/Ejercicio9 Clases/src/Pricipal.java
@@ -27,17 +27,25 @@ public class Pricipal {
 		
 		Vehiculo vehiculo3 = new Vehiculo();
 		vehiculo3.imprimir();
-		vehiculo3.setPatente("KPLT40");
+		vehiculo3.setPatente("KPLT40"); // set para modificar el valor actual del atributo
 		vehiculo3.setMarca("BWM");
 		vehiculo3.setColor("Blanco");
 		vehiculo3.imprimir();
 		
 		vehiculo2.setPatente("AF7010");
 		vehiculo2.imprimir();
+		
+		System.out.println("\nBúsqueda de la patente: KPLT40" );
+		System.out.println("La marca del vehiculo es: " + vehiculo3.getMarca());
+		
 	}
-/*
- * Agregar a la clase Cuenta getter and setter, encapsular, modificar datos del obj
+/* Ejercicio 20:
  * 
+ * Agregar a la clase Cuenta getter and setter y encapsular 
+ * Agregar contructor sin parametros
+ * 
+ * Crear un objeto sin parametro
+ * modificar datos del obj
  * 
  */
 }
